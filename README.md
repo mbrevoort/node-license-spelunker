46783b4304fa883ca6262bf98801317e6959754f
Mike Brevoort <mike.brevoort@pearson.com>
2013-09-17 14:29:24 -0600

    initial checking, simple license report

---
+++ README.md
@@ -0,0 +1,79 @@
+Attempt to find the licenses of all of you node.js dependencies recursively and 
+create a simple report that includes the license details.
+
+## Install
+
+	npm install -g license-spelunker
+
+## Usage
+
+	license-spelunker <path to node project>
+
+For example, from within a project
+
+	license-spelunker
+
+To a file
+
+	license-spelunker > license-report.txt
+
+The output will look something like this:
+
+	Project Path /Users/mikebre/development/licnese-spelunker
+	LICENSE FILE REPORT FOR  licnese-spelunker
+	2 nested dependencies
+	0 without identifiable license(s)
+
+
+	1 ----------------------------------------------------------------------------
+	licnese-spelunker@0.0.0
+	http://npmjs.org/package/licnese-spelunker
+
+	"MIT"
+
+	The MIT License (MIT)
+
+	Copyright (c) 2013 Mike Brevoort
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+	2 ----------------------------------------------------------------------------
+	async@0.2.9
+	http://npmjs.org/package/async
+	node_modules/async
+
+	Copyright (c) 2010 Caolan McMahon
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,134 @@
+var path = require('path')
+  , fs = require('fs')
+  , async = require('async')
+  ;
+
+
+var projPath = path.resolve(process.argv[2] || '.');
+console.log('Project Path', projPath);
+var topPkg = require(path.join(projPath, 'package.json'));
+
+var modules = [];
+var count = 0;
+
+doLevel(projPath);
+
+
+function doLevel(nodePath) {
+  var pkg = require(path.join(nodePath, 'package.json'));
+  var nodeModulesPath = path.join(nodePath, 'node_modules');
+  count ++;
+
+  //console.log('package.json license', pkg.license);
+
+  fs.exists(nodeModulesPath, function (dirExists) {
+    if (dirExists) {
+      fs.readdir(nodeModulesPath, function (err, files) {
+        if (err) throw err;
+        var directories = [];
+
+        files = files.map(function (f) { return path.join(nodeModulesPath, f); })
+        async.filter(files, isModuleDirectory, function(directories){
+          //console.log('module directories', directories);
+          directories.forEach(doLevel);
+        });
+      })
+    }
+  })
+
+  licenseText(nodePath, function (license) {
+    modules.push({
+      name: pkg.name,
+      version: pkg.version,
+      url: 'http://npmjs.org/package/' + pkg.name,
+      localPath: path.relative(projPath,nodePath),
+      pkgLicense: pkg.license,
+      license: license
+    });
+    count--;
+
+    if (count == 0) {
+      console.log('LICENSE FILE REPORT FOR ', topPkg.name);
+      console.log(modules.length + ' nested dependencies')
+      console.log(modules.filter(function (m) { return m.license === 'NO LICENSE FILE' }).length +  ' without identifiable license(s)', '\n\n')
+      modules.forEach(function(m) {
+        console.log((modules.indexOf(m)+1) + ' ----------------------------------------------------------------------------');
+        console.log(m.name + '@' + m.version);
+        console.log(m.url);
+        console.log(m.localPath);
+        if (m.pkgLicense) console.log(JSON.stringify(m.pkgLicense));
+        console.log('');
+        console.log(m.license);
+        console.log('');
+      })
+    }
+  })
+
+
+}
+
+function licenseText (nodePath, cb) {
+  var possibleLicensePaths = [ path.join(nodePath, 'LICENSE'),
+                               path.join(nodePath, 'LICENCE'),
+                               path.join(nodePath, 'LICENSE.md'),
+                               path.join(nodePath, 'LICENSE.txt'),
+                               path.join(nodePath, 'Readme.md'),
+                               path.join(nodePath, 'README.md')];
+
+ var emptyState  = "NO LICENSE FILE";
+
+  async.reduceRight(possibleLicensePaths, emptyState, function (state, licensePath, reduceCb) {
+    var isAReadme = (licensePath.toLowerCase().indexOf('/readme') > 0);
+
+    // if we already found a licnese, don't bother looking at READMEs
+    if (state !== emptyState && isAReadme) return reduceCb (null, state);
+
+    fs.exists(licensePath, function (exists) {
+      if (!exists) return reduceCb(null, state);
+      fs.readFile(licensePath, { encoding: 'utf8' }, function (err, text) {
+        if (err) return logError(err, reduceCb)(err, state);
+
+        if (isAReadme) {
+          var match = text.match(/\n.*license[ \t]+\n/i);
+          if (match) {
+            //console.log(match.input.substring(match.index))
+            return reduceCb (null, 'FROM README:\n' + match.input.substring(match.index));
+          }
+          else {
+            return reduceCb(null, state);
+          }
+        }
+        else {
+          return reduceCb (null, text);
+        }
+
+
+        return reduceCb (null, text);
+      })
+
+    });
+  }, function (err, license) {
+    if (err) return cb('ERROR FINDING LICENSE FILE ' + err );
+    cb (license);
+  });
+}
+
+function isModuleDirectory (dirPath, cb) {
+  var pkgPath = path.join(dirPath, 'package.json');
+  fs.stat(dirPath, function (err, stat) {
+    if (err) return logError(err, cb)(false);
+
+    var isdir = stat.isDirectory();
+    if (isdir) {
+      fs.exists(pkgPath, cb);
+    }
+    else {
+      cb(false);
+    }
+  });
+}
+
+function logError(err, cb) {
+  console.log('ERROR', err);
+  return cb
+}
